9acbbe1f5198aa36d420271e4e2155f2fa631058
DESKTOP-UCN82LQ\Илья <ilya.karnilov2@gmail.com>
2022-08-07 19:01:54 +0300

    Connect pizza on pizza page with checkout page

--- RazorWebApp/RazorWebApp/Pages/Pizza.cshtml.cs
+++ RazorWebApp/RazorWebApp/Pages/Pizza.cshtml.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorWebApp.Models;
 
 namespace RazorWebApp.Pages;
 
 public class Pizza : PageModel
 {
+    public List<PizzaModel> PizzaDB = new List<PizzaModel>()
+    {
+        new PizzaModel(){PizzaName = "Bolognese", ImageTitle = "Bolognese", Cheese = true, Meat = true, Mushrooms = false, Tomato = true, BasePrice = 12 },
+        new PizzaModel(){PizzaName = "Carbonara", ImageTitle = "Carbonara", Cheese = true, Meat = false, Mushrooms = false, Tomato = true, BasePrice = 9 },
+        new PizzaModel(){PizzaName = "Hawaiian", ImageTitle = "Hawaiian", Cheese = false, Meat = true, Mushrooms = true, Tomato = true, BasePrice = 11},
+        new PizzaModel(){PizzaName = "Margerita", ImageTitle = "Margerita", Cheese = true, Meat = true, Mushrooms = false, Tomato = true, BasePrice = 12},
+        new PizzaModel(){PizzaName = "MeatFeast", ImageTitle = "MeatFeast", Cheese = true, Meat = true, Mushrooms = false, Tomato = false, BasePrice = 10},
+        new PizzaModel(){PizzaName = "Vegetarian", ImageTitle = "Vegetarian", Cheese = false, Meat = false, Mushrooms = true, Tomato = true, BasePrice = 8}
+    };
     public void OnGet()
     {
         
